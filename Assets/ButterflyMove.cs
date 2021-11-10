505b8a3de3702b5099a6ab387f6249b3c9debb7d
Spencer <spencer_baty@stu.indianhills.edu>
2021-11-10 13:53:39 -0600

    v4 playerObejct
    now with better physics.

--- Assets/ButterflyMove.cs
+++ Assets/ButterflyMove.cs
@@ -6,13 +6,14 @@ public class ButterflyMove : MonoBehaviour
 {
     public float speed = 1f;
     public GameObject prefab;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
     
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -34,10 +35,15 @@ public class ButterflyMove : MonoBehaviour
         {
             transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
         }
-        if (Input.GetKey(KeyCode.W))
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.W))
         {
-            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+            float jumpVelocity = 10f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+
         }
+
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(speed * -Time.deltaTime, 0, 0);
@@ -47,5 +53,12 @@ public class ButterflyMove : MonoBehaviour
             transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         }
 
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down * .1f);
+        Debug.Log(raycastHit2d);
+        return raycastHit2d.collider != null;
     }
 }
